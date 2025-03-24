4d8221ff65248ceeadece624674ac7995c19c91a
hala kablaoui <hala.kablaoui@stu.ibu.edu.ba>
2025-03-25 00:16:35 +0100

    more cute

--- js/color_flipper/index.js
+++ js/color_flipper/index.js
@@ -1,7 +1,11 @@
 const body=document.getElementsByTagName('body')[0];
 
+
 function setColor(color){
+    body.style.backgroundImage='none';
     body.style.backgroundColor=color;
+    document.getElementById('texting').innerHTML='bg color is '+color;
+
 }
 function randomColor(){
     const red = Math.round(Math.random()*225)
@@ -9,5 +13,12 @@ function randomColor(){
     const blue = Math.round(Math.random()*225)
 
     const color=`rgb(${red},${green},${blue})`
+    body.style.backgroundImage='none';
     body.style.backgroundColor=color;
+    document.getElementById('texting').innerHTML='bg color is random';
+
+}
+function restarting(){
+    document.getElementById('texting').innerHTML='bg color is reset';
+    body.style.backgroundImage='url(sakuras.jpg)';
 }
